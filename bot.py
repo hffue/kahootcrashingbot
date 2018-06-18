4edfad0c57808fba46c82fe6f7807aa9abfde58a
cymug <37985852+cymug@users.noreply.github.com>
2018-06-17 20:47:54 -0400

    Version 0.0.3 of the bot
    - Added code for splitting titles
    - Added code to process each post based on title syntax
     - Added DB.close() in the savesubmission function
    - Added simple reply function that includes game PIN, chosen name, and bot amount
    - Polished some stuff, (line breaks, debug message formatting)
    
    - Removed (renamed?) stringsplit function, since processtitle takes care of splitting title, while runbot handles each post
    - Removed unnecessary  print(submission.title) statements

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ def init():
 		DB.close()
 
 def main():
-	print ('Logging in')
+	print ('Logging in\n')
 	reddit = praw.Reddit(username = config.username,
 			password = config.password,
 			client_id = config.client_id,
@@ -19,18 +19,27 @@ def main():
 	
 	subreddit = reddit.subreddit(config.target_subreddit)		
 	for submission in subreddit.stream.submissions():
-		if stringsplit(submission.title) and submissioncheck(submission):
+		proctitle = processtitle(submission)
+		if proctitle and submissioncheck(submission):
 			savesubmission(submission)
-			runbot(submission)
+			runbot(submission, proctitle)
+			print('Successfully ran bot on game [', submission.title, ']\n\n','--------------------------------------------------------------------------------\n')
+		
+		if proctitle and not submissioncheck(submission):
+			print('[',submission.title,'] has already been processed\n\n','--------------------------------------------------------------------------------\n')
+		
+		if not proctitle and submissioncheck(submission):
+			savesubmission(submission)
+			print('[',submission.title,'] was too short, ignoring post\n\n','--------------------------------------------------------------------------------\n')
+		
+		if not proctitle and not submissioncheck(submission):
+			print('[',submission.title,'] was both too short and already processed\n\n','--------------------------------------------------------------------------------\n')
 
-def runbot(submission):
-	print(submission.title)
-	print('Replying to: ',submission.title)
-	submission.reply(submission.title)
-
-def stringsplit(submission):
-	return True
-
+def runbot(submission, proctitle):
+	reply = ('Attempting to flood game [' + proctitle[0] + '] with [' + proctitle[2] + '] bots named [' + proctitle[1] + ']')
+	submission.reply(reply)
+	print('Replied to:',submission.title,'\n')
+	
 def submissioncheck(submission):
 	DB = open("processed.txt", "r")
 	IDs = DB.read()
@@ -44,9 +53,19 @@ def submissioncheck(submission):
 def savesubmission(submission):
 	DB = open("processed.txt", "a")
 	DB.write('[' + submission.id + ' , "' + submission.title + '"] - ')
-	print('Saved submission "', submission.title, '"')
+	print('Saved submission [', submission.title, ']\n')
+	DB.close()
+
+def processtitle(submission):
+	processedtitle = submission.title.split(" | ", 4)
+	shorttitle = processedtitle[:3]
+	if len(shorttitle) == 3:
+		return processedtitle
+	else:
+		return False
 
 init()
 
 if __name__ == '__main__':
 	main()
+
