0513e43e4da39a7c431ddc5b44f75bd5cdbc99b6
cymug <37985852+cymug@users.noreply.github.com>
2019-02-14 21:31:46 -0500

    Create forward.py
    Forwards the contents of the bot's inbox to my main account.

--- forward.py
+++ forward.py
@@ -1,1 +1,54 @@
+import praw
+import config
 
+def init():
+	try:
+		msgDB = open("forwarded.txt", "r")
+		msgDB.close()
+	except:
+		msgDB = open("forwarded.txt", "w+")
+		msgDB.close()
+
+def main():
+	print ('Loading forwarding bot\n')
+	reddit = praw.Reddit(username = config.username,
+			password = config.password,
+			client_id = config.client_id,
+			client_secret = config.client_secret,
+			user_agent = config.user_agent)
+	
+	for message in reddit.inbox.stream():
+		if messagecheck(message):
+			print("Forwarding message: ")
+			print("Subject: " + message.subject)
+			print("Body: " + message.body)
+			print("Sent by: u/" + str(message.author))
+			savemessage(message)
+			subject = message.subject
+			body = ("Message: " + message.body + "\n \n From u/" + str(message.author))
+			reddit.redditor('PMMEURTHROWAWAYS').message(subject, body)
+
+def messagecheck(message):
+	msgDB = open("forwarded.txt", "r")
+	msgIDs = msgDB.read()
+	if message.id in msgIDs:
+		msgDB.close()
+		return False
+
+	msgDB.close()
+	return True
+
+def savemessage(message):
+	try:
+		msgDB = open("forwarded.txt", "a")
+		msgDB.write('[' + message.id + ' , "' + message.subject + '"] - ')
+		msgDB.close()
+	except:
+		msgDB = open("forwarded.txt", "a")
+		msgDB.write('[' + message.id + ' , ERROR: Could not display part of message] - ')
+		msgDB.close()
+
+init()
+
+if __name__ == '__main__':
+	main()
