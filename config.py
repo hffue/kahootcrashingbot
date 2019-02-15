3807bb44a7770fd0cbc416315dde21b035e42f02
cymug <37985852+cymug@users.noreply.github.com>
2019-02-14 21:42:45 -0500

    Version 0.1.0 of the bot
    - Added debugging options
    
    - Updated user agent version

--- config.py
+++ config.py
@@ -2,5 +2,10 @@ username = "username"
 password = "password"
 client_id = "clientid"
 client_secret = "clientsecret"
-user_agent = "KahootCrasher v0.0.3 (by /u/PMMEURTHROWAWAYS)"
-target_subreddit = "subreddit"
+user_agent = "KahootCrasher v0.1.0 (by /u/PMMEURTHROWAWAYS)"
+target_subreddit = "subreddit(s)"
+
+#DEBUGGING
+comments = True
+join = True
+save = True
